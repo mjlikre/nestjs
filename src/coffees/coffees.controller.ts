048f7b212286e7e309886aab4988830a80102cbd
Michael Jiang <michaelPersonal@XinSens-Mac-mini.local>
2021-10-30 21:54:34 -0600

    chapter 3 done

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,6 +7,7 @@ import { Body,
     Post, 
     Query 
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -16,13 +17,13 @@ export class CoffeesController {
     constructor(private readonly coffeesServece: CoffeesService) {}
 
     @Get()
-    findAll(@Query() paginationQuery) {
+    findAll(@Query() paginationQuery: PaginationQueryDto) {
         // const { limit, offset } = paginationQuery;
-        return this.coffeesServece.findAll();
+        return this.coffeesServece.findAll(paginationQuery);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: number) {
+    findOne(@Param('id') id: string) {
         return this.coffeesServece.findOne(id) ;
     }
     
@@ -32,12 +33,12 @@ export class CoffeesController {
     }
 
     @Patch(':id')
-    update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
         return this.coffeesServece.update(id, updateCoffeeDto);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: number) {
+    remove(@Param('id') id: string) {
         return this.coffeesServece.remove(id)
     }
 }
